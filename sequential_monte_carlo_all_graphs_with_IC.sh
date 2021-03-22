c8c58fbe2b0e75a8d276d93391727eb799b12a92
anuragdutt <anurag2709@gmail.com>
2021-03-22 04:50:53 -0400

    Adding bash execution files for sequential monte carlo

---
+++ sequential_monte_carlo_all_graphs_with_IC.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+for i in {0..56..1}
+  do 
+     echo "running graph $i "
+     python sequential_monte_carlo_all_graphs_with_IC.py "$i" 1000
+ done
